2885f3c092872429c848ea315623772e59cc270b
Radu Schirliu <radu.schirliu@gmail.com>
2020-06-09 18:08:10 -0600

    Fixed inserting into timeline

--- src/routes/timeline.ts
+++ src/routes/timeline.ts
@@ -5,25 +5,23 @@ import { v4 as uuid } from 'uuid';
 
 const router = Router();
 
-router.get('/', async (req, res) => {
+router.get('/', (req, res) => {
   query('SELECT * FROM timeline_posts')
     .then((dbRes: QueryResult<any>) => {
-      res.json({
-        data: dbRes.rows,
-      });
+      res.json(dbRes.rows);
     })
     .catch((err: any) => {
       res.status(500).json({ error: err });
     });
 });
 
-router.post('/', async (req, res) => {
+router.post('/', (req, res) => {
   const data = {
     id: uuid(),
     title: req.body?.title || null,
     description: req.body?.description || '',
     link: req.body?.link || null,
-    date: new Date(req.body?.date) || null,
+    date: new Date(req.body?.date || null),
   };
 
   if (data.title == null || data.date == null) {
@@ -32,12 +30,11 @@ router.post('/', async (req, res) => {
   }
 
   query(
-    'INSERT INTO timeline_posts (id, title, description, link, date) VALUES ($1, $2, $3, $4, $5);',
+    'INSERT INTO timeline_posts (id, title, description, link, date) VALUES ($1, $2, $3, $4, $5) RETURNING *;',
     [data.id, data.title, data.description, data.link, data.date.toISOString()],
   )
     .then((dbRes: QueryResult<any>) => {
-      console.log(dbRes.rows);
-      res.json({ good: 'yes' });
+      res.json(dbRes.rows[0])
     })
     .catch((err: any) => {
       res.status(500).json({ error: err });
