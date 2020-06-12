abfde6978e02befc71ef0a7272d8fdaabc29630e
Radu Schirliu <radu.schirliu@gmail.com>
2020-06-11 22:27:59 -0600

    fix: Fixed the things

--- src/util/auth.ts
+++ src/util/auth.ts
@@ -1,6 +1,6 @@
 const requiredPassword = process.env.PASSWORD;
 
-export const requireAuth = (req, res, next) => {
+export const requireAuth = (req: any, res: any, next: any): any => {
   const authorization = req.headers?.authorization || null;
 
   if (!authorization) {
@@ -12,7 +12,6 @@ export const requireAuth = (req, res, next) => {
   }
 
   const [bearer, password] = authorization.split(' ');
-  console.log(requiredPassword);
   if (bearer !== 'Bearer' || password !== requiredPassword) {
     res.status(401).json({
       error: 'Not authorized'
