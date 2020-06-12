64c5d153647ab49ef928c396db748acb97cab362
Alexa Calkhoven <alexa.calkhoven1@ucalgary.ca>
2020-06-12 16:08:47 -0600

    Made city generation automatic when submitting a timelineitem post

--- src/routes/timeline.ts
+++ src/routes/timeline.ts
@@ -3,12 +3,26 @@ import { query } from '../util/db';
 import { QueryResult } from 'pg';
 import { v4 as uuid } from 'uuid';
 import { requireAuth } from '../util/auth';
-//import TimelineItem from '../models/timelinePost';
+import { addCity } from './city';
 
 const router = Router();
 
+async function checkForCity(name) {
+  const cities = await query('SELECT name FROM cities;');
+  //if city is in db, do nothing
+  for (const i in cities.rows) {
+    if (name == i) {
+      return;
+    }
+  }
+    //if city is not in db, create it
+  addCity(name);
+}
+
 router.get('/', (req, res) => {
-  query('SELECT * FROM timeline_posts INNER JOIN cities ON timeline_posts.cityname = cities.name;')
+  query(
+    'SELECT * FROM timeline_posts INNER JOIN cities ON timeline_posts.cityname = cities.name;',
+  )
     .then((dbRes: QueryResult<any>) => {
       res.json(dbRes.rows);
     })
@@ -22,7 +36,7 @@ router.post('/', requireAuth, (req, res) => {
     id: uuid(),
     title: req.body?.title || null,
     text: req.body?.text || '',
-    cityname: req.body?.cityname || null, 
+    cityname: req.body?.cityname || null,
     imageURL: req.body?.imageURL || null,
     newsURL: req.body?.newsURL || null,
     date: new Date(req.body?.date || null),
@@ -33,12 +47,22 @@ router.post('/', requireAuth, (req, res) => {
     return;
   }
 
+  checkForCity(data.cityname);
+
   query(
     `INSERT INTO timeline_posts (id, title, text, cityname, newsURL, imageURL, date) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;`,
-    [data.id, data.title, data.text, data.cityname, data.newsURL, data.imageURL, data.date.toISOString()],
+    [
+      data.id,
+      data.title,
+      data.text,
+      data.cityname,
+      data.newsURL,
+      data.imageURL,
+      data.date.toISOString(),
+    ],
   )
     .then((dbRes: QueryResult<any>) => {
-      res.json(dbRes.rows[0])
+      res.json(dbRes.rows[0]);
     })
     .catch((err: any) => {
       res.status(500).json({ error: err });
